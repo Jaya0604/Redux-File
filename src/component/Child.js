1a86a7cbba988090bdc4471ce5d4474aa9fee47f
Jaya0604 <jayashanthi064@gmail.com>
2024-07-01 16:31:03 +0530

    Added some text in to parent.js component and child component

--- src/component/Child.js
+++ src/component/Child.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function Child({chooseMessage}){
-    return(
-        <div >
-            <button onClick={()=> chooseMessage("God byes")}>Click Me</button>
-        </div>
-    )
+// function Child({chooseMessage}){
+//     return(
+//         <div >
+//             <button onClick={()=> chooseMessage("God byes")}>Click Me</button>
+//         </div>
+//     )
 
+// }
+// export default Child;
+
+function Child(props){
+    return(
+        <div>
+            {props.message}
+        </div>
+    );
 }
 export default Child;
--- src/component/Parent.js
+++ src/component/Parent.js
@@ -2,18 +2,28 @@ import React from 'react'
 import { useState } from "react";
 import Child from "./Child";
 
-function Parent(){
-    const [message, setMessage] = useState("Hello World");
-    function chooseMessage(msg){
-        setMessage(msg)
-    }
-    return(
-        <div>
+// function Parent(){
+//     const [message, setMessage] = useState("Hello World");
+//     function chooseMessage(msg){
+//         setMessage(msg)
+//     }
+//     return(
+//         <div>
             
-            <p>{message}</p>
-            <Child chooseMessage={chooseMessage}/>
-        </div>
-    )
+//             <p>{message}</p>
+//             <Child chooseMessage={chooseMessage}/>
+//         </div>
+//     )
 
+// }
+// export default Parent;
+
+function Parent(){
+    const data ="i am from Parent component";
+    return(
+<div>
+    <Child message={data}/>
+</div>
+    );
 }
-export default Parent;
+export default Parent
