9f22cce3d584326b85a5cb825e90e9029b4283c1
Jaya0604 <jayashanthi064@gmail.com>
2024-07-18 15:26:11 +0530

    Added some code

--- src/jsonData.js
+++ src/jsonData.js
@@ -88,16 +88,128 @@
 //  console.log(Emplyees);
 // console.log(Emplyees[0].id);
 
-const Findindex = (index_array) =>{
-	 index_array = index_array.sort((a,b)=> a-b);
-	 let dublicate_array =[];
-	 for(index in index_array){
-		if(index_array[index]=== index_array[index -1]){
-			dublicate_array.push(index_array[index]);
+// const Findindex = (index_array) =>{
+// 	 index_array = index_array.sort((a,b)=> a-b);
+// 	 let dublicate_array =[];
+// 	 for(index in index_array){
+// 		if(index_array[index]=== index_array[index -1]){
+// 			dublicate_array.push(index_array[index]);
+// 		}
+// 	 }
+// 	 return[...new Set(dublicate_array)];
+// 	}
+// let array =[1,1,2,2,3,4,5,5,6,6,7,7,8,8,9,3,4,65,];
+// console.log(Findindex(array));
+
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// process.stdin.on("data", function (input) {
+    
+//     for (i=1; i<=100; i++) {
+//     if (i%15 === 0) {
+//     console.log ("FizzBuzz");
+// }       
+//     else if (i%3 === 0) {
+//     console.log ("Fizz"); 
+// }
+//     else if (i%5 === 0) {
+//     console.log("Buzz");
+// }
+            
+//     else {
+//     console.log(i);
+// }    
+// }
+
+// });
+
+// function fizzBuzz(n) { 
+//     // Declare an array to store the results 
+//     let result = []; 
+  
+//     // Loop from 1 to n (inclusive) 
+//     for (let i = 1; i <= n; ++i) { 
+      
+//         // Check if i is divisible by both 3 and 5 
+//         if (i % 3 === 0 && i % 5 === 0) { 
+          
+//             // Add "FizzBuzz" to the result array 
+//             result.push("FizzBuzz"); 
+//         }  
+          
+//         // Check if i is divisible by 3 
+//         else if (i % 3 === 0) { 
+          
+//             // Add "Fizz" to the result array 
+//             result.push("Fizz"); 
+//         }  
+          
+//         // Check if i is divisible by 5 
+//         else if (i % 5 === 0) { 
+          
+//             // Add "Buzz" to the result array 
+//             result.push("Buzz"); 
+//         }  
+//         else { 
+          
+//             // Add the current number as a string to the 
+//             // result array 
+//             result.push(i.toString()); 
+//         } 
+//     } 
+  
+//     // Return the result array 
+//     return result; 
+// } 
+  
+// // Driver code 
+// let n = 20; 
+  
+// // Call the fizzBuzz function to get the result 
+// let result = fizzBuzz(n); 
+  
+// // Print the result 
+// console.log(result.join(' ')); 
+
+function Fizzbuszz(m){
+	let output = [];
+
+	for(let i =1; i <= m; i++){
+		if(i % 3 === 0 && i % 5 === 0){
+			output.push("Fizzbuszz");
+		}else if(i % 3 === 0 ){
+			output.push("Fizz");
+		}else if(i % 5 === 0){
+			output.push("buszz");
+		}else{
+			output.push(i.toString());
 		}
-	 }
-	 return[...new Set(dublicate_array)];
 	}
-let array =[1,1,2,2,3,4,5,5,6,6,7,7,8,8,9,3,4,65,];
-console.log(Findindex(array));
- 
+	return output;
+}
+let m = 40;
+let output = Fizzbuszz(m);
+console.log(output.join(' '));
+
+
+
+
+
+function Bassdata(j){
+	let name=[];
+	for(let i=1; i<=j; i++){
+		if(i % 3 === 0 && i % 5 === 0){
+			name.push("First");
+		}else if(i % 3 === 0){
+			name.push("Secound");
+		}else if(i % 5 === 0){
+			name.push("Thired");
+		}else{
+			name.push(i.toString());
+		}
+	}
+	return name;
+}
+ let j = 30;
+ let name=Bassdata(j);
+ console.log(name.join(' '));
